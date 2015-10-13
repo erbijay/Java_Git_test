f9b329fe6a6a7e908ab054c45eef5a0d936858bf
erbijay <java.bhandari85@gmail.com>
2015-10-13 15:11:20 -0500

    new class car created...

---
+++ Git_Test/src/com/bijay/git/personinfo/Car.java
@@ -0,0 +1,21 @@
+package com.bijay.git.personinfo;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c= new Car();
+		
+		c.getCar();
+		System.out.println("THE END");
+
+	}
+	
+	public void getCar(){
+		System.out.println("I need a Car-->BMW");
+		
+		//return null;
+		
+	}
+
+}
